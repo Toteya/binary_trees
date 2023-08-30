a911617edddc74a321913abd629a1dce29bcc4cd
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-30 22:08:16 +0200

    Left-rotate a binary tree

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - Performs a left-rotation on a binary_tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Pointer to the new root node of the binary tree
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *old_root, *new_root;
+
+	if (!tree)
+		return (NULL);
+
+	old_root = tree;
+	new_root = tree->right;
+	old_root->parent = new_root;
+	old_root->right = new_root->left;
+	new_root->left = old_root;
+
+	return (new_root);
+}
